adb44a975c52872ac7478ce1738ac21cba62dbac
Gabor Szabo <gabor@szabgab.com>
2019-03-10 14:58:07 +0200

    remove old pages redirect from them

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 import os
-#import jinja2
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
-
-#root = os.path.dirname(__file__)
-#my_loader = jinja2.ChoiceLoader([
-#        app.jinja_loader,
-#        jinja2.FileSystemLoader([root])
-#])
-#app.jinja_loader = my_loader
 
 @app.route("/<string:name>")
 def page(name):
-    return serve(name)
+    if name in ['clients.html', 'gabor.html', 'perl.html', 'development.html', 'staff.html', 'contact.html', 'infrastructure.html']:
+        return redirect('/', code=302)
 
 @app.route("/")
 def home():
-    return serve('index.html')
-
-
-def serve(filename):
-    return render_template(filename)
+    return render_template('index.html')
 
--- test_app.py
+++ test_app.py
@@ -4,3 +4,8 @@ def test_app():
     me = app.app.test_client()
     rv = me.get('/')
     assert b'<div>Host Local Training</div>' in rv.data
+
+
+    rv = me.get('/clients.html')
+    assert rv.status == '302 FOUND'
+    assert rv.headers['Location'] == 'http://localhost/'

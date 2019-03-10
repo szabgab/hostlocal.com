66d507538f519f86a7d8652cde4d05cdbf95b65a
Gabor Szabo <gabor@szabgab.com>
2019-03-10 17:35:52 +0200

    move course content to json file

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import os
-from flask import Flask, render_template, redirect
+import json
+from flask import Flask, render_template, redirect, abort
 app = Flask(__name__)
+
+root = os.path.dirname( __file__ )
 
 @app.route("/robots.txt")
 def robots():
     return ''
 
+@app.route("/eng/")
+def eng():
+    return redirect('/', code=302)
+
+@app.route("/eng/<string:name>")
+def course(name):
+    course_file = os.path.join(root, 'courses', 'eng', name + '.json')
+    if os.path.exists(course_file):
+        with open(course_file) as fh:
+            course = json.load(fh)
+        return render_template('course.html', course = course)
+    abort(404)
+
 @app.route("/<string:name>")
 def page(name):
     if name in ['clients.html', 'gabor.html', 'perl.html', 'development.html', 'staff.html', 'contact.html', 'infrastructure.html']:
         return redirect('/', code=302)
+    abort(404)
+
 
 @app.route("/")
 def home():
--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ def test_app():
     me = app.app.test_client()
 
     rv = me.get('/')
-    assert b'<div>Host Local Training</div>' in rv.data
+    assert b'<div><a href="/">Host Local Training</a></div>' in rv.data
 
 
     rv = me.get('/clients.html')
@@ -13,4 +13,12 @@ def test_app():
 
     rv = me.get('/robots.txt')
     assert b'' == rv.data
+
+    rv = me.get('/eng/other')
+    assert rv.status == '404 NOT FOUND'
+    #assert b'' == rv.data
+
+    rv = me.get('/eng/')
+    assert rv.status == '302 FOUND'
+    assert rv.headers['Location'] == 'http://localhost/'
 

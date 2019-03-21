9a959fb9f24d3655ccbda741f469bfe5dd36feff
Gabor Szabo <gabor@szabgab.com>
2019-03-21 21:06:36 +0200

    registration form

--- lunch.py
+++ lunch.py
@@ -1,9 +1,26 @@
 import os
 import json
-from flask import Flask, render_template, redirect, abort
+from validate_email import validate_email
+from flask import Flask, render_template, redirect, abort, request
 lunch = Flask(__name__)
+
+@lunch.route("/")
+def root():
+    return redirect('/lunch')
+
 
 @lunch.route("/lunch/")
 def home():
-    return 'Lunch'
+    return render_template('lunch/index.html')
+
+
+@lunch.route("/lunch/register", methods=['GET', 'POST'])
+def register():
+    email = request.form.get('email')
+    if email:
+        if validate_email(email):
+            return 'sent to ' + email
+        else:
+            return "Invalid e-mail address"
+    return render_template('lunch/register.html')
 
--- test_lunch.py
+++ test_lunch.py
@@ -4,6 +4,22 @@ def test_app():
     me = lunch.lunch.test_client()
 
     rv = me.get('/lunch/')
-    assert b'Lunch' == rv.data
+    assert b'<h1>Lunch</h1>' in rv.data
 
+
+    rv = me.get('/lunch/register')
+    assert b'Register' in rv.data
+
+
+    rv = me.post('/lunch/register', data=dict(
+        email='foo@bar..com'
+    ))
+    assert b'Invalid e-mail address' == rv.data
+
+
+    email = 'foo@bar.com'
+    rv = me.post('/lunch/register', data=dict(
+        email=email
+    ))
+    assert b'sent' in rv.data
 

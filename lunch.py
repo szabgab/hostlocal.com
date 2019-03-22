5373364ce2b0269367ce3c5bec9d5ed8906564a8
Gabor Szabo <gabor@szabgab.com>
2019-03-22 09:25:36 +0200

    save registration on our new one-json-file database

--- lunch.py
+++ lunch.py
@@ -1,8 +1,17 @@
 import os
+import time
 import json
+import random
+import string
+import uuid
 from validate_email import validate_email
 from flask import Flask, render_template, redirect, abort, request
 lunch = Flask(__name__)
+
+from mydb import database
+
+
+db_file = 'db.json'
 
 @lunch.route("/")
 def root():
@@ -22,5 +31,27 @@ def register():
 
     if not validate_email(email):
         return "Invalid e-mail address"
+
+    code = str(int(time.time())) + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+
+    uid = str(uuid.uuid1())
+
+    with database(db_file) as data:
+        data['users'][uid] = {
+            'email': email,
+            'verify_code': code,
+            'verify_code_timestamp': time.time(),
+        }
+
+    sendmail(email, code)
     return 'sent to ' + email
+
+@lunch.route("/lunch/verify/<code>")
+def verify(code):
+    return 'verified'
+
+def sendmail(to, code):
+    pass
+
+
 
--- test_lunch.py
+++ test_lunch.py
@@ -1,6 +1,22 @@
 import lunch
+import os
 
-def test_app():
+_to = None
+_code = None
+
+def _capture_mail(to, code):
+    global _to
+    global _code
+    _to = to
+    _code = code
+
+lunch.sendmail = _capture_mail
+
+def test_app(tmpdir):
+    tdir = str(tmpdir)
+    print(tdir)
+    lunch.db_file = os.path.join(tdir, 'test_db.json')
+
     me = lunch.lunch.test_client()
 
     rv = me.get('/lunch/')
@@ -17,9 +33,20 @@ def test_app():
     assert b'Invalid e-mail address' == rv.data
 
 
+   # TODO test that we remove spaces and make the e-mail lowercase
+
+
     email = 'foo@bar.com'
     rv = me.post('/lunch/register', data=dict(
         email=email
     ))
     assert b'sent' in rv.data
+    assert email == _to
+    print(_code)
 
+    rv = me.get('/lunch/verify/' + _code)
+    assert b'verified' in rv.data
+
+
+   # TODO: check that we send out an e-mail with a unique code in a link to be clicked
+   # TODO: click on the link to make the e-mail verified and give access to the site

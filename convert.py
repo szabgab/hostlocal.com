dd3a43641bd753989d8d99d06c7daeba8ab1cbf4
Gabor Szabo <gabor@szabgab.com>
2020-11-03 08:35:18 +0200

    convertor

---
+++ convert.py
@@ -0,0 +1,105 @@
+import re
+import os
+import sys
+import json
+
+# convert my JSON format to the pseudo-XML format used by Brend
+
+def main():
+    if len(sys.argv) != 2:
+        exit(f"Usage: {sys.argv[0]} FILENAME")
+
+    infile = sys.argv[1]
+    outfile = re.sub(r'\.json', '.xml', os.path.basename(infile))
+    template = get_template()
+
+    with open(infile) as fh:
+        data = json.load(fh)
+    template = re.sub(r'\{\{LANGUAGE}}',         'en', template)
+    template = re.sub(r'\{\{MAINTITLE}}',        'Python Courses:', template)
+    template = re.sub(r'\{\{TITLE}}',            data['title'], template)
+    template = re.sub(r'\{\{ABSTRACT}}',         '\n'.join(data['text']), template)
+    template = re.sub(r'\{\{KEYWORDS}}',         ', '.join(data['keywords']), template)
+    template = re.sub(r'\{\{SLOGAN}}',           '', template)
+    template = re.sub(r'\{\{SLOGAN2}}',          '', template)
+    template = re.sub(r'\{\{METADESCRIPTION}}',  '', template)
+    template = re.sub(r'\{\{COST}}',             '', template)
+    template = re.sub(r'\{\{TARGET}}',           '\n'.join(data['target_audience']), template)
+
+    details = ''
+    for part in data['syllabus']:
+        details += part['title'] + '\n'
+        details += '<ul>\n'
+        for row in part['entries']:
+            details += '<li>' + row + '</li>\n'
+        details += '</ul>\n'
+    template = re.sub(r'\{\{DETAILS}}',          details, template)
+    with open(outfile, 'w') as fh:
+        fh.write(template)
+    #print(template)
+    print(data.keys())
+
+
+
+def get_template():
+    return '''<LANGUAGE>
+{{LANGUAGE}}
+</LANGUAGE>
+<TITLE>
+{{TITLE}}
+</TITLE>
+<MAINTITLE>
+{{MAINTITLE}}
+</MAINTITLE>
+<SECTION>
+Python
+</SECTION>
+<ABSTRACT>
+{{ABSTRACT}}
+</ABSTRACT>
+<KEYWORDS>
+{{KEYWORDS}}
+</KEYWORDS>
+<METADESCRIPTION>
+{{METADESCRIPTION}}
+</METADESCRIPTION>
+<SLOGAN>
+{{SLOGAN}}
+</SLOGAN>
+<SLOGAN2>
+{{SLOGAN2}}
+</SLOGAN2>
+<TARGET>
+{{TARGET}}
+</TARGET>
+<PRICE_GROUP>
+medium
+</PRICE_GROUP>
+<COSTS>
+{{COST}}
+</COSTS>
+<DETAILS>
+{{DETAILS}}
+Of course, we can tailor this course to meet
+your specific goals.  Please, don't hesitate to contact us.
+</DETAILS>
+<ADDITIONAL>
+Comprehensive material
+</ADDITIONAL>
+<THUMBNAIL>
+green_python130.png
+</THUMBNAIL>
+<THUMBNAIL-TXT>
+Green Tree Python, Public Domain, excerpt from original image from pixabay.com
+</THUMBNAIL-TXT>
+<IMAGE>
+green_python.png
+</IMAGE>
+<IMAGE-TXT>
+Green Tree Python, Public Domain, original from pixabay.com
+</IMAGE-TXT>
+'''
+
+
+main()
+
